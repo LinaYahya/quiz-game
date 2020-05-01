0cfa003da12eef5fccc1e5aa2c75a64414a13713
Lina Ebeid <linaebe0@gmail.com>
2020-05-01 17:20:48 +0300

    quiz form created to display questions

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Category from './components/Category'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Category/>
+    </div>
+  );
+}
+
+export default App;
